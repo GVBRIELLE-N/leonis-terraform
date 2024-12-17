fa9070b2a40af54a9151fb67cadd52a13c2efdfa
GVBRIELLE-N <kayleigh.kasitra@gmail.com>
2024-12-17 11:02:23 +0200

    Added simple functionality for instancing

--- addons/leonis_world_gen/scripts/terrain_generator.gd
+++ addons/leonis_world_gen/scripts/terrain_generator.gd
@@ -2,3 +2,26 @@
 class_name EditorTerrainNode extends Node3D
 
 @export var HeightMapTexture : Texture2D
+
+var terrain_lod_0 : MeshInstance3D
+var terrain_lod_1 : MeshInstance3D
+var terrain_lod_2 : MeshInstance3D
+
+func _ready():
+	generate_terrain_mesh()
+
+func generate_terrain_mesh():
+	if get_child_count() > 0:
+		for child in get_children():
+			child.free()
+	generate_lod_0()
+	add_child(terrain_lod_0)
+	
+func generate_lod_0():
+	terrain_lod_0 = MeshInstance3D.new()
+	
+	var lod_0_mesh = PlaneMesh.new()
+	lod_0_mesh.subdivide_depth = 8
+	lod_0_mesh.subdivide_width = 8
+	
+	terrain_lod_0.mesh = lod_0_mesh
